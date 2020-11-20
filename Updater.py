25b3075822d9953fa83ba6fd5146d2ba0eb8abee
john garst <johnr@dominionintegration.com>
2020-11-20 17:38:15 +0000

    Adding Updater

--- Updater.py
+++ Updater.py
@@ -1,5 +1,14 @@
 from git import Repo
 
-repo = Repo('/home/pi/Desktop/PythonBoat')
 
-repo.remotes.origin.pull()
+#A very simple class for updating the boat remotely.
+#The idea is that we should be able to run the pi in
+#headless mode, plugged into ethernet, and have it update
+#correctly before running the actual Boatbrain.py program.
+
+try:
+    repo = Repo('/home/pi/Desktop/PythonBoat')
+    repo.remotes.origin.pull()
+except Exception as e:
+    print(str(e))
+
